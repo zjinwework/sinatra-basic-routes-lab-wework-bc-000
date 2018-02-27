34f80b636705093665af28c398ea1f87bee09d9f
Zhen Jin <zhen.jin@wework.com>
2018-02-27 02:45:12 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+  
+  get '/name' do 
+    "My name is Zhen"
+  end
+  
+  get '/hometown' do 
+    "My hometown is Aojiang"
+  end 
+  
+  get '/favorite-song' do 
+    "My favorite song is Shape of You"
+  end 
+  
 end
